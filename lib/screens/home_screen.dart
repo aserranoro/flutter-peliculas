5d7ec50e4d34161fded97be85f811bf718dd96a3
aserranoro <andresserranorodriguez25@gmail.com>
2023-01-04 14:03:04 +0100

    Fin seccion 7 flutter

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/providers/movies_provider.dart';
+import 'package:peliculas/search/search_delegate.dart';
 import 'package:peliculas/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Películas en cines'),
         elevation: 0,
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search_outlined))
+          IconButton(
+            icon: const Icon(Icons.search_outlined),
+            onPressed: () => showSearch(context: context, delegate: MovieSearchDelegate()),
+          )
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            // Tarjetas principales
-            CardSwiper(),
+            // Tarjetas principales 
+            CardSwiper(movies: moviesProvider.onDisplayMovies),
 
             // Slider de películas
-            MovieSlider()
+            MovieSlider(
+              movies: moviesProvider.popularMovies,
+              title: 'Populares', //opcional
+              onNextPage: () => moviesProvider.getPopularMovies(),
+            ),
           ],
         ),
       )
